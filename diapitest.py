c4252fd928e7525a21e1813af4f9e12404c07c46
Daniel Julio Iglesias Hambálek <daniel.julio.iglesias@gmail.com>
2018-10-08 19:26:36 +0300

    [INI] v1.0

---
+++ diapitest.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from config import Config
+import json
+import requests
+
+
+# proxies = {
+#     'http': 'http://username:password@Proxyadresse:Proxyport',
+#     'https': 'http://username:password@Proxyadresse:Proxyport',
+# }
+
+
+class APITest:
+    def __init__(self, api_key=None):
+        self.api_key = api_key
+        self.base_uri = 'http://httpbin.org'
+
+    def get(self):
+        uri = self.base_uri + '/get'
+        print(uri)
+        r = requests.get(self.base_uri + '/get')
+        # r = requests.get(self.base_uri + '/get', proxies=proxies)
+        return r.json()
+
+
+def main():
+    # print("Hello World!")
+    api_test = APITest()
+    print(api_test.get())
+
+    # r = requests.get('http://httpbin.org/get', proxies=proxies)
+    # print(r)
+
+
+if __name__ == '__main__':
+    main()
--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == '__main__':
+    main()
